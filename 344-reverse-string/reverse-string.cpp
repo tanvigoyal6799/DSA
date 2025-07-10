8d75460a2a460303813f7232bfef01bf5c20b60b
tanvigoyal6799 <tanvi6799@gmail.com>
2025-07-10 23:12:11 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (80.05%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void string(vector<char>& s, int left, int right) {
+        if (left >= right) return;
+        swap(s[left], s[right]);
+        string(s, left + 1, right - 1);
+    }
+
+
+    void reverseString(vector<char>& s) {
+        string(s, 0, s.size() - 1);
+    }
+    
+};
