f3e4633553492a94a1b11e9a237f38b1b4625917
tanvigoyal6799 <tanvi6799@gmail.com>
2025-07-11 13:18:25 +0530

    Time: 14 ms (22.24%) | Memory: 7.9 MB (36.58%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int series(int n){
+        if(n==0)return 0;
+        if(n==1)return 1;
+        return series(n-1)+series(n-2);
+    }
+    int fib(int n) {
+     return  series(n);   
+    }
+};
