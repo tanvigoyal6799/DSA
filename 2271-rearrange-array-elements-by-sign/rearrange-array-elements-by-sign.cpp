564f69d87e7d0b4ac6482dd1f0e09367e7f92ca0
tanvigoyal6799 <tanvi6799@gmail.com>
2025-08-18 03:19:30 -0300

    Time: 8 ms (51.70%) | Memory: 128.2 MB (79.78%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,27 +1,51 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int left=0;
+        int pos=0,neg=1;
         int n=nums.size();
-        vector<int> pos, neg;
+        vector<int> result(n);
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>0)
+            {
+                result[pos]=nums[i];
+                pos+=2;
+            }
+            else{            
+                result[neg]=nums[i];
+                neg+=2;
+            }
+        }
+        return result;
+
+
+
+
+
+
+
+    //     int left=0;
+    //     int n=nums.size();
+    //     vector<int> pos, neg;
 
     
-    for (int x : nums) {
-        if (x >= 0) pos.push_back(x);
-        else neg.push_back(x);
-    }
+    // for (int x : nums) {
+    //     if (x >= 0) pos.push_back(x);
+    //     else neg.push_back(x);
+    // }
 
-    int i = 0, j = 0, k = 0;
-
-    
-    while (i < pos.size() && j < neg.size()) {
-        nums[k++] = pos[i++];
-        nums[k++] = neg[j++];
-    }
+    // int i = 0, j = 0, k = 0;
 
     
+    // while (i < pos.size() && j < neg.size()) {
+    //     nums[k++] = pos[i++];
+    //     nums[k++] = neg[j++];
+    // }
+
+    
 
 
-    return nums;
+    // return nums;
     }   
 };
