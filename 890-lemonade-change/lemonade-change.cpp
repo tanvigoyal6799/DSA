ca99fe92982d9cb0c3efbc44ce0b197527579b67
tanvigoyal6799 <tanvi6799@gmail.com>
2025-11-15 23:11:36 +0530

    Time: 0 ms (100.00%) | Memory: 87.3 MB (39.56%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+       
+        int five = 0, ten = 0;
+
+        for(int b : bills) {
+
+            if(b == 5) five++;
+
+            else if(b == 10) {
+                if(five == 0) return false;
+                five--;
+                ten++;
+            }
+
+            else { // b == 20
+                if(ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+                else if(five >= 3) {
+                    five -= 3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+};
+
