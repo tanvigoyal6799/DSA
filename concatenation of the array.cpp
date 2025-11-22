36fcec2291082a163ee8080c4f7c1446a697b219
tanvigoyal6799 <tanvi6799@gmail.com>
2025-11-22 18:02:18 +0530

    Implement array concatenation in Solution class

---
+++ concatenation of the array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans;
+        ans.reserve(2 * n);
+
+        for(int i = 0; i < n; i++) {
+            ans.push_back(nums[i]);
+            
+        }
+        for(int i = 0; i < n; i++) {
+            ans.push_back(nums[i]);
+            
+        }
+
+        return ans;
+    }
+};
