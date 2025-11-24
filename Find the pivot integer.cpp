0bb30dca98570d0e75406bc7b17d898c61279872
tanvigoyal6799 <tanvi6799@gmail.com>
2025-11-25 00:00:02 +0530

    Create Find the pivot integer.cpp

---
+++ Find the pivot integer.cpp
@@ -0,0 +1,21 @@
+lass Solution {
+public:
+    int pivotInteger(int n) {
+        int total=0;
+        for(int i=0;i<=n;i++)
+        {
+            total=total+i;
+        }
+        int left=0;
+        for(int i=1;i<=n;i++)
+        {
+            int right= total-left-i;
+            if(right==left)
+            {
+                return i;
+            }
+            left+=i;
+        }
+        return -1;
+    }
+};
