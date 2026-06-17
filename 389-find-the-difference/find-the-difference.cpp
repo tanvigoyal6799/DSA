13adb1aa83a3c1f909d73f0261f4298675e2ef01
tanvigoyal6799 <tanvi6799@gmail.com>
2026-06-18 01:07:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.89%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
+        // xor 
         int ans=0;
         for(char c : s)
         {
@@ -11,5 +12,25 @@ public:
             ans^=c;
         }
         return ans;
+
+
+        // using hash
+
+        unordered_map<int, char> result;
+        for(char c: s)
+        {
+            result[c]++;
+        }
+
+        for(char c: t)
+        {
+            result[c]--;
+            if(result[c]<0)
+            {
+                return c;
+            }
+            return ' ';
+        }
+        
     }
 };
