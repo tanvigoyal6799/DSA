c209a8dd16886f1ab5fd5ee1a108aa4d8d06146c
tanvigoyal6799 <tanvi6799@gmail.com>
2026-07-06 23:57:38 +0530

    Time: 7 ms (20.69%) | Memory: 17.1 MB (8.41%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -3,11 +3,9 @@ public:
     string frequencySort(string s) {
          unordered_map<char, int> freq;
 
-        // Count frequency
         for (char c : s)
             freq[c]++;
 
-        // Bucket where index = frequency
         vector<vector<char>> bucket(s.size() + 1);
 
         for (auto it : freq)
@@ -15,7 +13,6 @@ public:
 
         string ans = "";
 
-        // Traverse from highest frequency
         for (int i = s.size(); i >= 1; i--) {
             for (char c : bucket[i]) {
                 ans.append(i, c);
