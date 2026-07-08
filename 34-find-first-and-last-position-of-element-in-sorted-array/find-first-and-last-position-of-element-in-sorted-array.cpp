3e7661003fb0f5e6a94b7c410781ab30833a95c3
tanvigoyal6799 <tanvi6799@gmail.com>
2026-07-08 23:35:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (19.51%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,12 +1,54 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-           int left = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-        int right = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
+int firstOccurrence(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = -1;
 
-        if (left == nums.size() || nums[left] != target)
-            return {-1, -1};
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-        return {left, right};
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1;   
+            }
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return ans;
+    }
+
+    int lastOccurrence(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1;    
+            }
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return ans;
+    }
+     vector<int> searchRange(vector<int>& nums, int target) {
+        return {firstOccurrence(nums, target),
+                lastOccurrence(nums, target)};
+    // vector<int> searchRange(vector<int>& nums, int target) {
+        //    int left = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        // int right = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
+
+        // if (left == nums.size() || nums[left] != target)
+        //     return {-1, -1};
+
+        // return {left, right};
     }
 };
