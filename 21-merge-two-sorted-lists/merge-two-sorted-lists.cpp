3f9be8d910c96398bee355b558c201a06e863f04
tanvigoyal6799 <tanvi6799@gmail.com>
2026-07-18 15:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (87.88%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,25 +11,37 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-         ListNode dummy;
-        ListNode* tail = &dummy;
+        //  ListNode dummy;
+        // ListNode* tail = &dummy;
 
-        while (list1 && list2) {
-            if (list1->val <= list2->val) {
-                tail->next = list1;
-                list1 = list1->next;
-            } else {
-                tail->next = list2;
-                list2 = list2->next;
-            }
-            tail = tail->next;
+        // while (list1 && list2) {
+        //     if (list1->val <= list2->val) {
+        //         tail->next = list1;
+        //         list1 = list1->next;
+        //     } else {
+        //         tail->next = list2;
+        //         list2 = list2->next;
+        //     }
+        //     tail = tail->next;
+        // }
+
+        // if (list1)
+        //     tail->next = list1;
+        // else
+        //     tail->next = list2;
+
+        // return dummy.next;
+
+
+        if (!list1) return list2;
+        if (!list2) return list1;
+
+        if (list1->val <= list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
         }
-
-        if (list1)
-            tail->next = list1;
-        else
-            tail->next = list2;
-
-        return dummy.next;
     }
 };
