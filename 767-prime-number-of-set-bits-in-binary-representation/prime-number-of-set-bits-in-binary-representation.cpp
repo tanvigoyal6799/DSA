30d8d03d99ebc63674e6dce12beaedc15b72aa00
tanvigoyal6799 <tanvi6799@gmail.com>
2026-08-05 00:24:16 +0530

    Time: 4 ms (44.08%) | Memory: 8.4 MB (11.10%) - LeetSync

---
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        unordered_set<int> prime = {2,3,5,7,11,13,17,19};
+        int ans = 0;
+
+        for (int i = left; i <= right; i++) {
+            if (prime.count(__builtin_popcount(i)))
+                ans++;
+        }
+
+        return ans;
+
+    }
+};
